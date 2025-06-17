42cf8611dd0e4680b6047006117ea392be8e6c76
khashiii97 <khashiii97@gmail.com>
2025-06-17 09:45:30 -0700

    local changes made

--- benchmark.py
+++ benchmark.py
@@ -198,6 +198,9 @@ if __name__ == "__main__":
     """
     args = parse_args()
 
+    if args.data_dir == 'cc': # setting the datadir to cc path
+        args.data_dir = '/home/kka151/scratch/WF_representation_learning_data/laserbeak'
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # load checkpoint (if it exists)
---
+++ jobs/adaptive_tamaraw.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=two_step_vs_rf   # Job name
+#SBATCH --gres=gpu:1                   # Request GPU resource
+#SBATCH --nodes=1                      # Run all processes on a single node
+#SBATCH --ntasks=1                     # Run a single task        
+#SBATCH --cpus-per-task=30              # Number of CPU cores per task
+#SBATCH --mem=128G                      # Job memory request
+#SBATCH --time=20:00:00                # Time limit hrs:min:sec
+#SBATCH --output=/home/kka151/projects/def-t55wang/kka151/Laserbeak-WF-Classifier/jobs/logs/tamaraw_two_step_lb_k_7_%j.log  # Standard output and error log
+
+
+# Load any modules and activate your conda environment here
+module load StdEnv/2023
+module load python/3.11.5
+module load rust/1.76.0
+module load gcc arrow/17.0.0
+source /home/kka151/venvs/python_11_5/bin/activate
+
+
+# Navigate to your project directory (optional)
+cd /home/kka151/projects/def-t55wang/kka151/Laserbeak-WF-Classifier
+
+
+
+# Execute your deep learning script
+python3 benchmark.py --data_dir cc --config ./configs/laserbeak.json --dataset adaptive-tamaraw --epochs 20 --multisamples 10 --exp_name my_experiment
